d2fc6fd917e04081cbdb960fdd52107574e5a365
Didar-jpg <guneshev.didar@gmail.com>
2024-08-12 17:52:51 +0500

    added handling Enter keyDown in component Input.jsx

--- src/components/Layouts/Header/Input.jsx
+++ src/components/Layouts/Header/Input.jsx
@@ -11,18 +11,19 @@ export default function Input() {
   const [ settetState, setSettleState ] = useRecoilState(settlementState)
   
   const changeSettlement = () => { 
+    console.log('aqaaa')
     const location = document.getElementById('settlement-input').value
     setSettleState(location)
   }
 
   return (
         <div className="relative w-3/4 sm:w-80">
-            <input id='settlement-input' type="text" className="w-full p-2 pl-8 rounded border border-gray-50 bg-gray-50 focus:bg-white focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent duration-200" placeholder={t("input_text")} defaultValue="" />
+            <input id='settlement-input' onKeyDown={e => { if(e.key === 'Enter') changeSettlement()}} type="text" className="w-full p-2 pl-8 rounded border border-gray-50 bg-gray-50 focus:bg-white focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent duration-200" placeholder={t("input_text")} defaultValue="" />
             <svg className="w-4 h-4 absolute left-2.5 top-[14px]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
             </svg>
 
-            <button onClick={changeSettlement} className="absolute right-3 top-[5px] py-1 px-2 bg-blue-50 rounded">
+            <button id='searchBtn' onClick={changeSettlement} className="absolute right-3 top-[5px] py-1 px-2 bg-blue-50 rounded">
               {t('search')}
             </button>
         </div>
