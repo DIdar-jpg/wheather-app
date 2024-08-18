21e5bb31b6b2bbe66b9333eb531135f507bca57f
Didar-jpg <guneshev.didar@gmail.com>
2024-08-18 16:11:55 +0500

    added urls to FooterIcons

--- src/components/Layouts/Footer/FooterIcons.jsx
+++ src/components/Layouts/Footer/FooterIcons.jsx
@@ -11,15 +11,35 @@ import { BiLogoGmail } from "react-icons/bi";
 
 export default function FooterIcons() {
     const icons = [
-        {icon: <FaTelegramPlane />, key:'telegram'},
-        {icon: <FaLinkedinIn />, key:'linkedIn'},
-        {icon: <FaInstagram />, key:'instagram'}, 
-        {icon: <FaWhatsapp />, key:'whatsUp'}, 
-        {icon: <BiLogoGmail />, key:'gmail'}
+        {
+            icon: <FaTelegramPlane />,
+            key:'telegram',
+            url: process.env.REACT_APP_TELEGRAM_URL
+        },
+        {
+            icon: <FaLinkedinIn />,
+            key:'linkedIn',
+            url: process.env.REACT_APP_LINKEDIN_URL
+        },
+        {
+            icon: <FaInstagram />,
+            key:'instagram',
+            url: process.env.REACT_APP_INSTAGRAM_URL
+        }, 
+        {
+            icon: <FaWhatsapp />, 
+            key:'whatsUp',
+            url: process.env.REACT_APP_WHATSAPP_URL
+        }, 
+        {
+            icon: <BiLogoGmail />,
+            key:'gmail',
+            url: process.env.REACT_APP_GMAIL_URL
+        }
     ]
     return (
         <div className="middle">
-            {icons.map( item => <div className="btn" key={item.key}>{item.icon}</div>)}
+            {icons.map( item => <a href={item.url} className="btn" key={item.key}>{item.icon}</a>)}
         </div>
     )
 }
